8f3955a6884df342d3e975be747320eca11ee310
thegautamrohit <rgautam1997@gmail.com>
2023-11-04 20:49:18 +0530

    update

--- Sorting/Problem2824.js
+++ Sorting/Problem2824.js
@@ -1,3 +1,9 @@
+// 2824. Count Pairs Whose Sum is Less than Target
+
+
+// Given a 0-indexed integer array nums of length n and an integer target,
+// return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+
 function mergeSort(arr) {
   if (arr.length < 2) {
     return arr;
