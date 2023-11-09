38f91c7ba8e96c35134937743f63497f66f97ce9
thegautamrohit <rgautam1997@gmail.com>
2023-11-09 20:59:36 +0530

    167

---
+++ TwoPointers/Problem167.js
@@ -0,0 +1,17 @@
+function twoSum(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
